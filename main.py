a147494c8143dfa96050dfcd543bb8ae1865db17
GeekVan <lychallengers@gmail.com>
2020-07-13 21:43:46 +0800

    完成项目，能够提取数据

--- main.py
+++ main.py
@@ -2,14 +2,10 @@
 import requests
 # 解析库
 from bs4 import BeautifulSoup
-# 用于解决爬取的数据格式化
-import io
-import sys
 
 
 def processWords(Link):
     r = requests.get(Link)
-    r.encoding = None
     result = r.text
     bs = BeautifulSoup(result, 'html.parser')
     root = bs.find_all('div', id='content-well_1-0')[0]
@@ -19,49 +15,51 @@ def processWords(Link):
                 if hasattr(child1, 'attrs') and child1.attrs['id'] == 'article-body_1-0':
                     for child2 in child1:
                         if hasattr(child2, 'attrs') and child2.attrs['id'] == 'mntl-sc-page_1-0':
-                            t = ""
-                            tag=0
                             for child3 in child2:
                                 # 两种情况的字符串拼接
                                 if hasattr(child3, 'attrs') and child3.attrs['id'] == 'mntl-sc-block_1-0':
+                                    t = ""
                                     for i in range(len(child3.contents)):
                                         if hasattr(child3.contents[i], 'attr'):
                                             t += " " + child3.contents[i].contents[0].strip()
                                         else:
                                             t += " " + child3.contents[i].strip()
                                 if hasattr(child3, 'attrs') and child3.attrs['id'] == 'mntl-sc-block_1-0-1':
-                                    t = ""
+                                    if len(child3.contents) == 0:
+                                        return t
+                                    else:
+                                        t = ""
                                     for i in range(len(child3.contents)):
-                                        t += " " + child3.contents[i].strip()
-                                    tag=1
-                                    break
-                            if tag==1:
-                                break
-    # Todo
-    # 输出有点问题
-    print(t)
+                                        if hasattr(child3.contents[i], 'attr'):
+                                            t += " " + str(child3.contents[i].contents[0]).strip()
+                                        else:
+                                            t += " " + str(child3.contents[i]).strip()
+                                    return t
 
 
-def getTermWords():
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-    # 爬取的网页链接
-    r = requests.get("https://www.investopedia.com/terms-beginning-with-a-4769351")
-    # 类型
-    # print(type(r))
+def getTermWords(text,link):
+    r = requests.get(link)
     print(r.status_code)
-    # 中文显示
-    # r.encoding='utf-8'
-    r.encoding = None
     result = r.text
     bs = BeautifulSoup(result, 'html.parser')
     divs = bs.find_all('div', id='dictionary-top300-list_1-0')
     divs = divs[0].contents[1].contents[1].contents
-    for i in range(len(divs)):
-        if i % 2 == 1 and 'href' in divs[i].attrs:
-            # 测试标签第9个,1,3,5,7,9
-            if i == 9:
-                processWords(divs[i].attrs['href'])
+    with open(text, "a",encoding='utf-8') as file:
+        for i in range(len(divs)):
+            if i % 2 == 1 and 'href' in divs[i].attrs:
+               file.write(divs[i].contents[0].contents[0]+":\n"+processWords(divs[i].attrs['href'])[1:]+"\n\n\n")
+
+def writeTxt():
+    linkPrefix='https://www.investopedia.com/terms-beginning-with-'
+    linkAftfix='-476'
+    lists=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s'
+           ,'t','u','v','w','x','y','z']
+    for i in range(len(lists)):
+        # i=0 -->'a'
+        # i=1 -->'b'
+        if i==25:#设定i获取对应的文件，全部采用for循环进程会卡住（网络的原因和网站的原因限制爬虫）
+            getTermWords('data/result-'+lists[i]+'.txt',linkPrefix+lists[i]+linkAftfix+str(i+9351))
 
 
 if __name__ == "__main__":
-    getTermWords()
+    writeTxt()
